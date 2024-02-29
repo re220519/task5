484e98d4c59a98fdc3094e034513379e79aaa816
re220519 <kreshma.k54@gmail.com>
2024-02-29 14:03:11 +0400

    initial commit

---
+++ backend/server.js
@@ -0,0 +1,25 @@
+const express= require("express");
+const cors=require("cors");
+
+const mysql= require ("mysql");
+
+const app= express();
+
+app.use(cors());
+
+const db=mysql.createConnection({
+    host: "localhost",
+    user:"root",
+    password: "",
+    database: "crud",
+})
+app.get("/", (req, res)=>{
+   const sql = "SELECT * FROM student";
+   db.query(sql, (err, data)=>{
+    if(err) return res.json("error");
+    return res.json(data);
+   })
+})
+app.listen(8081, () =>{
+    console.log("listening");
+  })
